e6f5507a8fe5c40320ab11de13e31f09867e82ee
tevinowino <tevinowino65@gmail.com>
2024-11-14 12:46:14 +0300

    UI and Interest Only Functionality

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,136 +1,117 @@
+import { useState } from "react";
+
 export const meta = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Mortgage app calculator" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
+  let [monthlyRepayments, setMonthlyRepayments] = useState('');
+  let [totalCost, setTotalCost] = useState('');
+  let [result, setResult] = useState(false);
+  let [mortgageAmount, setMortgageAmount] = useState('');
+  let [mortgageTerm, setMortgageTerm] = useState('');
+  let [interestRate, setInterestRate] = useState('');
+  let [mortgageType, setMortgageType] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResult(true);
+    const principal = parseFloat(mortgageAmount);
+    const monthlyInterestRate = parseFloat(interestRate) / 100 / 12;
+    let monthlyInterestPayment = principal * monthlyInterestRate;
+
+    setMonthlyRepayments(monthlyInterestPayment.toFixed(2));
+    setTotalCost((monthlyInterestPayment * 12 * parseInt(mortgageTerm)).toFixed(2));
+
+    console.log({ monthlyRepayments, totalCost });
+  };
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
+    <main className="bg-[#e3f4fc] min-h-screen py-40 px-10">
+      <div className="bg-white flex flex-col sm:flex-row rounded-md">
+        <section className="w-full sm:w-1/2 p-7">
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-2xl font-bold text-gray-800">Mortgage Calculator</h2>
+            <button className="border-b-[2px] border-[#13304040] text-[#133040] font-semibold">
+              Clear All
+            </button>
           </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-          <ul>
-            {resources.map(({ href, text, icon }) => (
-              <li key={href}>
-                <a
-                  className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {icon}
-                  {text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <TextInput label="Mortgage Amount" onChange={(e) => setMortgageAmount(e.target.value)} />
+            </div>
+            <div className="flex justify-between items-center gap-4 mb-4">
+              <TextInput label="Mortgage Term" onChange={(e) => setMortgageTerm(e.target.value)} />
+              <TextInput label="Interest Rate" onChange={(e) => setInterestRate(e.target.value)} />
+            </div>
+            <div className="mb-4">
+              <fieldset className="flex flex-col gap-4">
+                <legend className="font-medium text-[#133040da]">Mortgage Type</legend>
+                <RadioInput label="Repayment" value="repayment" onChange={(e) => setMortgageType(e.target.value)} />
+                <RadioInput label="Interest Only" value="interestOnly" onChange={(e) => setMortgageType(e.target.value)} />
+              </fieldset>
+            </div>
+            <button type="submit" className="bg-[#d9db30] hover:bg-[#d8db30a1] py-3 px-5 rounded-3xl font-bold">
+              Calculate Repayments
+            </button>
+          </form>
+        </section>
+        <section className="w-full sm:w-1/2 bg-[#133040] p-8 rounded-bl-[70px] shadow-md text-white flex flex-col items-center">
+          {result ? (
+            <div className="text-center w-full max-w-xs mx-auto">
+              <h3 className="text-4xl font-semibold mb-6">Results</h3>
+              <div className="bg-[#1a4b73] p-5 rounded-lg shadow-inner">
+                <p className="text-lg mb-2"><span className="font-bold">Mortgage Amount:</span> ${mortgageAmount}</p>
+                <p className="text-lg mb-2"><span className="font-bold">Term:</span> {mortgageTerm} years</p>
+                <p className="text-lg mb-2"><span className="font-bold">Interest Rate:</span> {interestRate}%</p>
+                <p className="text-lg mb-2"><span className="font-bold">Type:</span> {mortgageType}</p>
+                <p className="text-xl mt-5 font-semibold">Monthly Interest Payment: ${monthlyRepayments}</p>
+                <p className="text-xl font-semibold">Total Interest Cost: ${totalCost}</p>
+              </div>
+            </div>
+          ) : (
+            <div className="text-center">
+              <h3 className="text-3xl font-semibold mb-4">Results</h3>
+              <p className="text-xl">Enter details to calculate the interest payment.</p>
+            </div>
+          )}
+        </section>
+
       </div>
-    </div>
+    </main>
   );
 }
 
-const resources = [
-  {
-    href: "https://remix.run/start/quickstart",
-    text: "Quick Start (5 min)",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M8.51851 12.0741L7.92592 18L15.6296 9.7037L11.4815 7.33333L12.0741 2L4.37036 10.2963L8.51851 12.0741Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://remix.run/start/tutorial",
-    text: "Tutorial (30 min)",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M4.561 12.749L3.15503 14.1549M3.00811 8.99944H1.01978M3.15503 3.84489L4.561 5.2508M8.3107 1.70923L8.3107 3.69749M13.4655 3.84489L12.0595 5.2508M18.1868 17.0974L16.635 18.6491C16.4636 18.8205 16.1858 18.8205 16.0144 18.6491L13.568 16.2028C13.383 16.0178 13.0784 16.0347 12.915 16.239L11.2697 18.2956C11.047 18.5739 10.6029 18.4847 10.505 18.142L7.85215 8.85711C7.75756 8.52603 8.06365 8.21994 8.39472 8.31453L17.6796 10.9673C18.0223 11.0653 18.1115 11.5094 17.8332 11.7321L15.7766 13.3773C15.5723 13.5408 15.5554 13.8454 15.7404 14.0304L18.1868 16.4767C18.3582 16.6481 18.3582 16.926 18.1868 17.0974Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://remix.run/docs",
-    text: "Remix Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
-  },
-];
+// Components
+const TextInput = ({ label, onChange }) => (
+  <div className="flex flex-col gap-1">
+    <label className="font-medium text-[#133040da]" htmlFor={label}>
+      {label}
+    </label>
+    <input
+      type="number"
+      id={label}
+      className="w-full rounded-md border-[#133040] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+      onChange={onChange}
+    />
+  </div>
+);
+
+const RadioInput = ({ label, value, onChange }) => (
+  <div className="p-2 rounded-md flex items-center gap-2 border-2 border-gray-300">
+    <input
+      type="radio"
+      id={value}
+      name="mortgage-type"
+      value={value}
+      className=""
+      onChange={onChange}
+    />
+    <label htmlFor={value} className="text-[#133040] font-bold">
+      {label}
+    </label>
+  </div>
+);
